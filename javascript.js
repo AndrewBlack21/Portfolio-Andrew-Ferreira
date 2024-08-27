64ac532996f9303fa3cc29b0d8378e5b7bfd6fbc
AndrewBlack21 <andrew.moraes16@hotmail.com>
2024-08-27 16:46:48 -0300

    atualizando html

--- javascript.js
+++ javascript.js
@@ -17,3 +17,27 @@ document.getElementById('verMais').addEventListener('click', function() {
       this.textContent = 'Ver mais';
   }
 });
+
+const habilidades = [
+    { descricao: "HTML5", arquivo: "html.logo.png" },
+    { descricao: "CSS3", arquivo: "css.logo.png" },
+    { descricao: "JavaScript", arquivo: "js.logo.png" },
+    { descricao: "Em aprendizado", arquivo: "coming_soon.png" },
+    { descricao: "Em aprendizado", arquivo: "coming_soon.png" },
+    { descricao: "Em aprendizado", arquivo: "coming_soon.png" },
+];
+
+const habilidadesSection = document.getElementById("habilidades");
+
+habilidades.forEach(habilidade => {
+    const habilidadeDiv = document.createElement("div");
+    habilidadeDiv.classList.add("habilidade");
+    habilidadeDiv.setAttribute("data-descricao", habilidade.descricao);
+
+    const img = document.createElement("img");
+    img.src = `imagens/${habilidade.arquivo}`;
+    img.alt = habilidade.descricao;
+
+    habilidadeDiv.appendChild(img);
+    habilidadesSection.appendChild(habilidadeDiv);
+});
