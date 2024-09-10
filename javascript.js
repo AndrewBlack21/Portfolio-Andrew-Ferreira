b2498d8edb77255a802c7602a07e1fb64ad8063b
AndrewBlack21 <andrew.moraes16@hotmail.com>
2024-09-10 20:27:27 -0300

    atualizando aba dos porjetos

--- javascript.js
+++ javascript.js
@@ -1,23 +1,3 @@
-document.getElementById('verMais').addEventListener('click', function() {
-  const projetos = document.querySelectorAll('.projeto.hidden');
-  
-  projetos.forEach(function(projeto) {
-      // Se o projeto estiver oculto, mostre-o; caso contrário, oculte-o novamente
-      if (projeto.style.display === 'none' || projeto.style.display === '') {
-          projeto.style.display = 'block';
-      } else {
-          projeto.style.display = 'none';
-      }
-  });
-
-  // Alterna o texto do botão entre "Ver mais" e "Ver menos"
-  if (this.textContent === 'Ver mais') {
-      this.textContent = 'Ver menos';
-  } else {
-      this.textContent = 'Ver mais';
-  }
-});
-
 // Adicionar imagens nas habilidades 
 const habilidades = [
     { descricao: "HTML5", arquivo: "html.logo.png" },
